a2e7e8eed7e199cfe5bd096dda9303c3259ec6b5
JohnDoenut <j.brammer@psych.ru.nl>
2019-02-27 18:35:00 +0100

    started writing benchmark test for resp_offline

--- get_bidmc.py
+++ get_bidmc.py
@@ -6,5 +6,27 @@ Created on Wed Feb 27 14:24:54 2019
 """
 
 import wfdb
+import matplotlib.pyplot as plt
+import numpy as np
+from resp_offline import extrema_signal
 
-records = wfdb.io.get_record_list('bidmc', records='all')
+records = wfdb.get_record_list('bidmc', records='all')
+
+for record in records[:3]:
+    
+    data = wfdb.rdrecord(record, pb_dir='bidmc')
+    annotation = wfdb.rdann(record, pb_dir='bidmc', extension='breath')
+    
+    sfreq = data.fs
+    resp_chan = data.sig_name.index('RESP,')
+    resp = data.p_signal[:, resp_chan]
+    peaks = annotation.sample
+    mypeaks, mytroughs, _, _ = extrema_signal(resp, sfreq)
+    
+    plt.figure()
+    plt.plot(resp)
+    plt.scatter(peaks, resp[peaks], c='r')
+    plt.scatter(mypeaks, resp[mypeaks], c='r', marker='X')
+    plt.scatter(mytroughs, resp[mytroughs], c='r', marker='X')
+    
+
