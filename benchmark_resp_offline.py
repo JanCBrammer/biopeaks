974c8801d6ce4e6aea44a41c684e46b5f5139d73
JohnDoenut <j.brammer@psych.ru.nl>
2019-03-01 12:12:39 +0100

    finished implementation of benchmarking for resp_offline

--- benchmark_resp_offline.py
+++ benchmark_resp_offline.py
@@ -16,7 +16,7 @@ sensitivity2 = []
 precision1 = []
 precision2 = []
 
-for record in records[:-1]:
+for record in records[:1]:
 
     data = wfdb.rdrecord(record, pb_dir='bidmc')
     annotation = wfdb.rdann(record, pb_dir='bidmc', extension='breath')
@@ -31,23 +31,28 @@ for record in records[:-1]:
     manupeaks1 = annotation.sample[annotator1]
     manupeaks2 = annotation.sample[annotator2]
     algopeaks, _, _, _ = extrema_signal(resp, sfreq)
-    
-#    plt.figure()
-#    plt.plot(resp)
-#    plt.scatter(manupeaks1, resp[manupeaks1], c='m')
-#    plt.scatter(manupeaks2, resp[manupeaks2], c='b')
-#    plt.scatter(algopeaks, resp[algopeaks], c='g', marker='X')
-    
-    
-    # perform benchmarking against each annotator seperately
-    # an algorythmically annotated peaks is scored as true positives if it is
-    # within 200ms of a manually annotated peak (Daluwatte et al. 2015)
-    
+
+    plt.figure()
+    plt.plot(resp)
+    plt.scatter(manupeaks1, resp[manupeaks1], c='m')
+    plt.scatter(manupeaks2, resp[manupeaks2], c='b')
+    plt.scatter(algopeaks, resp[algopeaks], c='g', marker='X', s=150)
+
+    # perform benchmarking against each annotator seperately; an
+    # algorythmically annotated peaks is scored as true positives if it is
+    # within 700 msec of a manually annotated peak, this relatively large
+    # window was chosen to account for the clipping that occurs in many of the
+    # recordings; clipping results in large palteaus that make placement of
+    # peak somewhat arbitrary (algorithm places peak at the edges while manual
+    # annotation places peak towards the middle); for non-clipped peaks, the
+    # agreement of manual and algorithmic annotation is within a range smaller
+    # than 700 msec (enable plotting to confirm)
+
     # unilateral extend of acceptance margin centered on each algopeak; in sec
-    acceptance_margin = 0.2 
+    acceptance_margin = 0.7
     tp1 = []
     tp2 = []
-    
+
     # true positives; unique peaks that are in both, algopeaks and manupeaks (
     # wuthih the specified acceptance margin)
     for algopeak in algopeaks:
@@ -61,8 +66,7 @@ for record in records[:-1]:
             tp1.append(algopeak)
         if mindiff2 <= acceptance_margin * sfreq:
             tp2.append(algopeak)
-            
-            
+
     # false positives: unique peaks in algopeaks that are not in true positives
     fp1 = np.setdiff1d(algopeaks, tp1)
     fp2 = np.setdiff1d(algopeaks, tp2)
@@ -70,20 +74,16 @@ for record in records[:-1]:
     # true positives
     fn1 = np.setdiff1d(manupeaks1, tp1)
     fn2 = np.setdiff1d(manupeaks2, tp2)
-    
+
+    # calculate two metrics for benchmarking (according to AAMI guidelines):
+    # 1. sensitivity: how many of the manually annotated peaks does the
+    # algorithm annotate as peaks (TP / TP + FN)?
+    # 2. precision: out of all peaks that are algorithmically annotated as
+    # peaks (TP + FP), how many are correct (TP)?
     sensitivity1.append(float(len(tp1)) / (len(fp1) + len(fn1)))
     precision1.append(float(len(tp1)) / (len(tp1) + len(fp1)))
     sensitivity2.append(float(len(tp2)) / (len(fp2) + len(fn2)))
     precision2.append(float(len(tp2)) / (len(tp2) + len(fp2)))
-    
+
 print np.mean(sensitivity1), np.mean(precision1)
 print np.mean(sensitivity2), np.mean(precision2)
-    
-    
-    # calculate two metrics for benchmarking (according to AAMI guidelines):
-    # 1. sensitivity: how many of the manually annotated peaks does the 
-    # algorithm annotate as peaks (TP / TP + FN)?
-    # 2. precision: out of all peaks that are algorithmically annotated as 
-    # peaks (TP + FP), how many are correct (TP)?
-    
-
