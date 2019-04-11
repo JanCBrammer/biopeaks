b30719ecb7553081217103a7aa5e96ec34975a9e
JohnDoenut <j.brammer@psych.ru.nl>
2019-04-11 21:10:26 +0200

    added first (toy) functionality

--- gui.py
+++ gui.py
@@ -6,17 +6,31 @@ Created on Thu Apr  4 18:52:52 2019
 """
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
- 
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+
 class Window(QWidget):
  
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('biopeaks')
-        self.show()
+        self.initUI()
  
+    def initUI(self):
+        self.setWindowTitle('biopeaks')
+        self.setGeometry(50, 50, 1000, 700)
+#        self.load_data()
+        self.load_button()
+        self.show()
+    
+    def load_button(self):
+        btn = QPushButton('Browse', self)
+        btn.clicked.connect(self.load_data)
+        
+    def load_data(self):
+        print('foo')  
+        
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Window()
+    GUI = Window()
     sys.exit(app.exec_())
+    
