743400184ab06c2e96f20013638106b53b5d34f7
JohnDoenut <j.brammer@psych.ru.nl>
2019-04-12 20:43:47 +0200

    started implementing toolbar and file dialog

--- gui.py
+++ gui.py
@@ -6,9 +6,11 @@ Created on Thu Apr  4 18:52:52 2019
 """
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,
+                             QFileDialog, QAction, QMainWindow)
+from PyQt5.QtGui import QIcon
 
-class Window(QWidget):
+class Window(QMainWindow):
  
     def __init__(self):
         super().__init__()
@@ -17,20 +19,27 @@ class Window(QWidget):
     def initUI(self):
         self.setWindowTitle('biopeaks')
         self.setGeometry(50, 50, 1000, 700)
-#        self.load_data()
-        self.load_button()
+        self.setWindowIcon(QIcon('python_icon.png'))
+        
+        openFile = QAction(QIcon('open_icon.png'), 'Open', self)
+        openFile.triggered.connect(self.open_dialog)
+        
+        toolbar = self.addToolBar('File')
+        toolbar.addAction(openFile)      
+        
         self.show()
     
-    def load_button(self):
-        btn = QPushButton('Browse', self)
-        btn.clicked.connect(self.load_data)
         
-    def load_data(self):
-        print('foo')  
+    def open_dialog(self):
+#        print('foo')
+        fname = QFileDialog.getOpenFileNames(self, 'Open file', '/home')
+        print(fname[0])
         
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     GUI = Window()
     sys.exit(app.exec_())
+    
+
     
