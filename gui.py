dcd0da42d66445af82cdb847206e66eb606ab8e6
JohnDoenut <j.brammer@psych.ru.nl>
2019-04-15 19:46:12 +0200

    added selecting file, loading and plotting data

--- gui.py
+++ gui.py
@@ -6,9 +6,17 @@ Created on Thu Apr  4 18:52:52 2019
 """
 
 import sys
+from guiutils import load_data
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,
-                             QFileDialog, QAction, QMainWindow)
+                             QFileDialog, QAction, QMainWindow,
+                             QVBoxLayout, QSizePolicy)
 from PyQt5.QtGui import QIcon
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import (FigureCanvasQTAgg as
+                                                FigureCanvas)
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as
+                                                NavigationToolbar)
+
 
 class Window(QMainWindow):
  
@@ -18,28 +26,42 @@ class Window(QMainWindow):
  
     def initUI(self):
         self.setWindowTitle('biopeaks')
-        self.setGeometry(50, 50, 1000, 700)
+        self.setGeometry(50, 50, 1500, 500)
         self.setWindowIcon(QIcon('python_icon.png'))
         
-        openFile = QAction(QIcon('open_icon.png'), 'Open', self)
-        openFile.triggered.connect(self.open_dialog)
-        
+        # set up toolbar
+        openData = QAction(QIcon('open_icon.png'), 'Open', self)
+        openData.triggered.connect(self.open_data)
         toolbar = self.addToolBar('File')
-        toolbar.addAction(openFile)      
+        toolbar.addAction(openData)      
+        
+        # set up the central widget containing the plot and navigationtoolbar
+        self.centwidget = QWidget()
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.navitools = NavigationToolbar(self.canvas, self)
+        self.layout = QVBoxLayout(self.centwidget)
+        self.layout.addWidget(self.canvas)
+        self.layout.addWidget(self.navitools)
+        self.setCentralWidget(self.centwidget)
         
         self.show()
-    
-        
-    def open_dialog(self):
-#        print('foo')
-        fname = QFileDialog.getOpenFileNames(self, 'Open file', '/home')
+
+    # toolbar methods
+    def open_data(self):
+        fname = QFileDialog.getOpenFileNames(self, 'Open file', '\home')
         print(fname[0])
-        
-    
+        data = load_data(fname[0][0][:-4])
+       
+        self.figure.clear()
+        ax = self.figure.add_subplot(111)
+        ax.plot(data)
+        self.canvas.draw()
+   
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     GUI = Window()
     sys.exit(app.exec_())
-    
-
     
