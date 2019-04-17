59647e59c26c3272b721d3d10905d0164944a48c
JohnDoenut <j.brammer@psych.ru.nl>
2019-04-17 18:40:13 +0200

    added functionality to find and plot peaks

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ Created on Thu Apr  4 18:52:52 2019
 
 import sys
 from guiutils import load_data
+from ecg_offline import peaks_signal
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,
                              QFileDialog, QAction, QMainWindow,
                              QVBoxLayout, QSizePolicy)
@@ -29,16 +30,25 @@ class Window(QMainWindow):
         self.setGeometry(50, 50, 1500, 500)
         self.setWindowIcon(QIcon('python_icon.png'))
         
+        # initialize data
+        self.data = None
+        
         # set up toolbar
+        toolbar = self.addToolBar('Toolbar')
+
         openData = QAction(QIcon('open_icon.png'), 'Open', self)
         openData.triggered.connect(self.open_data)
-        toolbar = self.addToolBar('File')
         toolbar.addAction(openData)      
-        
+
+        findPeaks = QAction(QIcon('plot_icon.png'), 'Find peaks', self)
+        findPeaks.triggered.connect(self.find_peaks)
+        toolbar.addAction(findPeaks)      
+
         # set up the central widget containing the plot and navigationtoolbar
         self.centwidget = QWidget()
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
+        self.ax = self.figure.add_subplot(111)
         self.navitools = NavigationToolbar(self.canvas, self)
         self.layout = QVBoxLayout(self.centwidget)
         self.layout.addWidget(self.canvas)
@@ -50,15 +60,21 @@ class Window(QMainWindow):
     # toolbar methods
     def open_data(self):
         fname = QFileDialog.getOpenFileNames(self, 'Open file', '\home')
-        print(fname[0])
-        data = load_data(fname[0][0][:-4])
-       
-        self.figure.clear()
-        ax = self.figure.add_subplot(111)
-        ax.plot(data)
-        self.canvas.draw()
-   
-
+        
+        # initiate plot to show user that their data loaded successfully
+        if fname[0]:
+            self.data = load_data(fname[0][0][:-4])
+            self.ax.clear()
+            self.ax.plot(self.data)
+            self.canvas.draw()
+        
+    def find_peaks(self):
+        
+        # show peaks
+        if self.data is not None:
+            self.peaks = peaks_signal(self.data, 360)
+            self.ax.scatter(self.peaks, self.data[self.peaks], c='m')
+            self.canvas.draw()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
