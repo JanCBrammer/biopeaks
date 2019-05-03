fbd742f53fdc5995ad131b35b44ae3e674811254
JohnDoenut <j.brammer@psych.ru.nl>
2019-05-03 18:26:19 +0200

    improved updating of gradient threshold

--- resp_online.py
+++ resp_online.py
@@ -10,16 +10,14 @@ import matplotlib.pyplot as plt
 from filters import butter_lowpass_filter
 
 
-def extrema_signal(signal, sfreq, enable_plot=True):
+def extrema_signal(signal, sfreq, enable_plot=False):
     '''
     the goal is to provide real-time estimates of a) breathing rate, and b)
-    the current phase of breathing (inspiration, expiration); a will be
-    estimated based on negative zero-crossings of the signals gradient (i.e.,
+    the current phase of breathing (inspiration, expiration); a is
+    estimated based on negative zero-crossings of the signal's gradient (i.e.,
     one breathing cycle is defined as peak-to-peak difference, since peaks are
-    more sharply defined with the Bitalino belt []); b) is simply the gradient
-    of the signal at any time;
-    free parameters: lrate, filterorder, filterboundary, weight gradthreshold,
-    weight horzdiff
+    more sharply defined than troughs with the Bitalino belt []); b) is simply
+    the gradient of the signal at any time
     
     '''
     
@@ -34,12 +32,13 @@ def extrema_signal(signal, sfreq, enable_plot=True):
     window_size = np.ceil(3 * sfreq)
     # amount of samples to shift the window on each iteration
     stride = np.ceil(1 * sfreq)
-    
+    extend = int(np.ceil(0.2 * sfreq))
+
     # initiate stuff
     block = 0
     last_extreme = 0
-    gradthresh = 0
-    lrate = 0.1
+    gradthresh = 0    
+    peaks = []
 
     while block * stride + window_size <= len(signal):
         block_idcs = np.arange(block * stride, block * stride + window_size, 
@@ -47,9 +46,7 @@ def extrema_signal(signal, sfreq, enable_plot=True):
         
         x = signal[block_idcs]
         # filter out fluctuations that occur faster than 0.5 Hz (breathing
-        # rate faster than 30), this implicitly ensures a minimum horizontal
-        # distance between peaks, no need to apply an additional threshold in
-        # time
+        # rate faster than 30)
         x_filt = butter_lowpass_filter(x, 0.5, sfreq, order=2)
         grad = np.gradient(x_filt)
         
@@ -62,30 +59,36 @@ def extrema_signal(signal, sfreq, enable_plot=True):
         # get negative zero crossings
         extrema = neg_crossings(grad)
                     
-        if extrema.size > 0 and enable_plot is True:
+        if extrema.size > 0:
             extreme = extrema[-1]
-            plt.scatter(block_idcs[extreme], x[extreme], c='y',
-                        marker='P', s=200)
             horzdiff = block_idcs[extreme] - last_extreme
+            if enable_plot is True:
+                plt.scatter(block_idcs[extreme], x[extreme], c='y', marker='P',
+                            s=200)
             # get average gradient in 200msec preceding each negative zero
             # crossing
-            extend = int(np.ceil(0.2 * sfreq))
             if grad[extreme - extend:extreme].size > 0:
                 avggrad = np.average(grad[extreme - extend:extreme])
-            
                 # if the current temporal difference exceeds a second, consider
                 # the current zero crossing a valid extreme; also the average 
                 # gradient preceding the extreme must exceeed a threshold
                 if (horzdiff > 1 * sfreq) and (avggrad > 0.3 * gradthresh):
-                    plt.scatter(block_idcs[extreme], x[extreme], c='m',
-                                marker='X', s=200)
-                    # update parameter
-                    gradthresh = (1 - lrate) * gradthresh + lrate * avggrad
-                    print(gradthresh)
+                    # update running mean of gradient threshold (Knuth; 
+                    # https://dsp.stackexchange.com/questions/811/determining-
+                    # the-mean-and-standard-deviation-in-real-time)
+                    gradthresh = (gradthresh + (avggrad - gradthresh) /
+                                  (block + 1))
+                    peaks.append(block_idcs[extreme])
+                    if enable_plot is True:
+                        plt.scatter(block_idcs[extreme], x[extreme], c='m',
+                                    marker='X', s=200)
 
-            last_extreme = block_idcs[extreme]
+            if peaks:
+                last_extreme = peaks[-1]
 
         block += 1
+        
+    return peaks
 
 def neg_crossings(signal):
         pos = signal > 0
