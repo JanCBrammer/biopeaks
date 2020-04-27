a462bffcc0870af07bf7b869a377f319a6da88d0
JohnDoenut <jan.c.brammer@gmail.com>
2020-04-27 15:56:44 +0200

    Rewrote tests for batch file processing mode using pytest-qt.

--- biopeaks/tests/test_gui.py
+++ biopeaks/tests/test_gui.py
@@ -42,7 +42,7 @@ ppg_os = {"modality": "PPG",
           "sigchan": "A1",
           "markerchan": "I1",
           "mode": "single file",
-          "sigpathorig": Path(datadir).joinpath("OSmontagePPG.txt"),
+          "sigpathorig": datadir.joinpath("OSmontagePPG.txt"),
           "sigfnameseg": "testdata_segmented.txt",
           "peakfname": "testdata_segmented_peaks.csv",
           "statsfname": "testdata_segmented_stats.csv",
@@ -60,7 +60,7 @@ ppg_edf = {"modality": "PPG",
            "sigchan": "A5",
            "markerchan": "A1",
            "mode": "single file",
-           "sigpathorig": Path(datadir).joinpath("EDFmontage0.edf"),
+           "sigpathorig": datadir.joinpath("EDFmontage0.edf"),
            "sigfnameseg": "testdata_segmented.edf",
            "peakfname": "testdata_segmented_peaks.csv",
            "statsfname": "testdata_segmented_stats.csv",
@@ -96,7 +96,7 @@ ecg_edf = {"modality": "ECG",
            "sigchan": "A3",
            "markerchan": "A1",
            "mode": "single file",
-           "sigpathorig": Path(datadir).joinpath("EDFmontage0.edf"),
+           "sigpathorig": datadir.joinpath("EDFmontage0.edf"),
            "sigfnameseg": "testdata_segmented.edf",
            "peakfname": "testdata_segmented_peaks.csv",
            "statsfname": "testdata_segmented_stats.csv",
@@ -114,7 +114,7 @@ rsp_os = {"modality": "RESP",
           "sigchan": "A2",
           "markerchan": "I1",
           "mode": "single file",
-          "sigpathorig": Path(datadir).joinpath("OSmontage0J.txt"),
+          "sigpathorig": datadir.joinpath("OSmontage0J.txt"),
           "sigfnameseg": "testdata_segmented.txt",
           "peakfname": "testdata_segmented_peaks.csv",
           "statsfname": "testdata_segmented_stats.csv",
@@ -133,7 +133,7 @@ rsp_edf = {"modality": "RESP",
            "sigchan": "A5",
            "markerchan": "A1",
            "mode": "single file",
-           "sigpathorig": Path(datadir).joinpath("EDFmontage0.edf"),
+           "sigpathorig": datadir.joinpath("EDFmontage0.edf"),
            "sigfnameseg": "testdata_segmented.edf",
            "peakfname": "testdata_segmented_peaks.csv",
            "statsfname": "testdata_segmented_stats.csv",
@@ -268,134 +268,104 @@ def test_singlefile(qtbot, tmpdir, cfg):
         assert np.around(stats["tidalamp"].mean(), 4) == cfg["avgtidalamp"]
 
 
-# def batch_file(self, assertion, modality, sigchan, mode, sigfnames,
-#            peakdir, statsdir, peaksums, stats, correctpeaks=False):
+ecg_batch = {"modality": "ECG",
+             "sigchan": "A3",
+             "mode": "multiple files",
+             "sigfnames": ["OSmontage1A.txt", "OSmontage1J.txt",
+                           "OSmontage2A.txt", "OSmontage2J.txt",
+                           "OSmontage3A.txt", "OSmontage3J.txt"],
+             "peaksums": [3808244, 3412308, 2645824, 3523449, 3611836,
+                          3457936],
+             "stats": [(0.7950, 76.1123), (0.7288, 83.1468),
+                       (0.7894, 76.8911), (0.7402, 81.7864),
+                       (0.7856, 76.9153), (0.7235, 83.6060)],
+             "correctpeaks": False}
 
-# # 1. set options and set paths
-# ##############################
-# QTest.keyClicks(self._view.modmenu, modality)
-# QTest.keyClicks(self._view.sigchanmenu, sigchan)
-# QTest.keyClicks(self._view.batchmenu, mode)
-# self._view.savecheckbox.setCheckState(Qt.Checked)
-# if correctpeaks:
-#     self._view.correctcheckbox.setCheckState(Qt.Checked)
-# self._view.periodcheckbox.setCheckState(Qt.Checked)
-# self._view.ratecheckbox.setCheckState(Qt.Checked)
-# if modality == "RESP":
-#     self._view.tidalampcheckbox.setCheckState(Qt.Checked)
-# self._model.fpaths = sigfnames
+ecg_batch_autocorrect = {"modality": "ECG",
+                         "sigchan": 'A3',
+                         "mode": "multiple files",
+                         "sigfnames": ["OSmontage1A.txt", "OSmontage1J.txt",
+                                       "OSmontage2A.txt", "OSmontage2J.txt",
+                                       "OSmontage3A.txt", "OSmontage3J.txt"],
+                         "peaksums": [3808204, 3394480, 2626445, 3511241,
+                                      3611829, 3457941],
+                         "stats": [(0.7944, 76.0998), (0.7308, 82.8572),
+                                   (0.7934, 76.2335), (0.7418, 81.5011),
+                                   (0.7856, 76.9152), (0.7235, 83.5973)],
+                         "correctpeaks": True}
 
-# # 2. process batch
-# ##################
-# # use a mockup of the controller's batch_processor in order to avoid
-# # calls to the controller's get_wpathpeaks and get_wpathstats methods
-# self._controller.methodnb = 0
-# self._controller.nmethods = 5
-# self._controller.filenb = 0
-# self._controller.nfiles = len(self._model.fpaths)
-
-# self._model.wdirpeaks = peakdir
-# self._model.wdirstats = statsdir
-
-# self._model.status = 'processing files'
-# self._model.plotting = False
-# self._model.progress_changed.connect(self._controller.dispatcher)
-
-# # initiate processing
-# self._controller.dispatcher(1)
-
-# # wait for all files to be processed
-# while self._controller.filenb < self._controller.nfiles:
-# #            print(self._controller.filenb, self._controller.nfiles)
-#     QTest.qWait(500)
-
-# # 3. check peaks
-# ################
-# # load each peak file saved during batch processing and assess if
-# # peaks have been identified correctly
-# for sigfname, peaksum in zip(sigfnames, peaksums):
-#     # load signal
-#     self._controller.read_signal(path=sigfname)
-#     self.wait_for_signal(self._model.progress_changed, 1)
-#     # load peaks
-#     fpartname, _ = os.path.splitext(sigfname)
-#     self._model.rpathpeaks = f"{fpartname}_peaks.csv"
-#     self._controller.read_peaks()
-#     self.wait_for_signal(self._model.progress_changed, 1)
-#     if assertion:
-#         assert sum(self._model.peaks) == peaksum, \
-#                 f"failed to load peaks for {sigfname}"
-#         print(f"loaded peaks for {sigfname} successfully")
-#     else:
-#         print(f"sum of peaks is {sum(self._model.peaks)}")
-#     # remove all files that have been saved during the test
-#     os.remove(self._model.rpathpeaks)
-#     self._model.reset()
-
-# # 3. check stats
-# ################
-# for sigfname, stat in zip(sigfnames, stats):
-#     fpartname, _ = os.path.splitext(sigfname)
-#     statsfname = f"{fpartname}_stats.csv"
-#     stats = pd.read_csv(statsfname)
-#     if assertion:
-#         assert np.around(stats["period"].mean(), 4) == stat[0], \
-#                 f"failed to save period for {sigfname}"
-#         assert np.around(stats["rate"].mean(), 4) == stat[1], \
-#                 f"failed to save rate for {sigfname}"
-#         print('saved statistics successfully')
-#     else:
-#         print(f"mean re-loaded period is {np.around(stats['period'].mean(), 4)}"
-#               f" mean re-loaded rate is {np.around(stats['rate'].mean(), 4)}")
-#     if modality == "RESP":
-#         if assertion:
-#             assert np.around(stats["tidalamp"].mean(), 4) == stat[2], \
-#                 f"failed to save tidalamplitude for {sigfname}"
-#         else:
-#             print(f"mean re-loaded amplitude is {np.around(stats['tidalamp'].mean(), 4)}")
-
-#     # remove all files that have been saved during the test
-#     os.remove(statsfname)
-
-# # restore original state of optionpanel
-# self._view.savecheckbox.setCheckState(Qt.Unchecked)
-# self._view.correctcheckbox.setCheckState(Qt.Unchecked)
-# self._view.periodcheckbox.setCheckState(Qt.Unchecked)
-# self._view.ratecheckbox.setCheckState(Qt.Unchecked)
-# if modality == "RESP":
-#     self._view.tidalampcheckbox.setCheckState(Qt.Unchecked)
+cfgs = [ecg_batch, ecg_batch_autocorrect]
 
 
-# # batch processing with OpenSignals ECG data
-# sigfiles = ['OSmontage1A.txt', 'OSmontage1J.txt', 'OSmontage2A.txt',
-#             'OSmontage2J.txt', 'OSmontage3A.txt', 'OSmontage3J.txt']
-# peaksums = [3808244, 3412308, 2645824, 3523449, 3611836, 3457936]
-# stats = [(0.7950, 76.1123), (0.7288, 83.1468), (0.7894, 76.8911),
-#           (0.7402, 81.7864), (0.7856, 76.9153), (0.7235, 83.6060)]
-# testapp._tests.batch_file(assertion=assertion,
-#                           modality='ECG',
-#                           sigchan='A3',
-#                           mode='multiple files',
-#                           sigfnames=sigfiles,
-#                           peakdir=datapath,
-#                           statsdir=datapath,
-#                           peaksums=peaksums,
-#                           stats=stats,
-#                           correctpeaks=False)
+def idcfg(cfg):
+    """Generate a test ID."""
+    modality = cfg["modality"]
+    if cfg["correctpeaks"]:
+        correction = "autocorrection"
+    else:
+        correction = "uncorrected"
+    return f"{modality}_{correction}"
 
-# # batch processing with OpenSignals ECG data with autocorrection of peaks
-# sigfiles = ['OSmontage1A.txt', 'OSmontage1J.txt', 'OSmontage2A.txt',
-#             'OSmontage2J.txt', 'OSmontage3A.txt', 'OSmontage3J.txt']
-# peaksums = [3808204, 3394480, 2626445, 3511241, 3611829, 3457941]
-# stats = [(0.7944, 76.0998), (0.7308, 82.8572), (0.7934, 76.2335),
-#          (0.7418, 81.5011), (0.7856, 76.9152), (0.7235, 83.5973)]
-# testapp._tests.batch_file(assertion=assertion,
-#                           modality='ECG',
-#                           sigchan='A3',
-#                           mode='multiple files',
-#                           sigfnames=sigfiles,
-#                           peakdir=datapath,
-#                           statsdir=datapath,
-#                           peaksums=peaksums,
-#                           stats=stats,
-#                           correctpeaks=True)
+
+@pytest.mark.parametrize("cfg", cfgs, ids=idcfg)
+def test_batchfile(qtbot, tmpdir, cfg):
+
+    # Set up application.
+    model = Model()
+    controller = Controller(model)
+    view = View(model, controller)
+    qtbot.addWidget(view)
+    view.show()
+
+    # Configure options.
+    qtbot.keyClicks(view.modmenu, cfg["modality"])
+    qtbot.keyClicks(view.sigchanmenu, cfg["sigchan"])
+    qtbot.keyClicks(view.batchmenu, cfg["mode"])
+    view.savecheckbox.setCheckState(Qt.Checked)
+    if cfg["correctpeaks"]:
+        view.correctcheckbox.setCheckState(Qt.Checked)
+    view.periodcheckbox.setCheckState(Qt.Checked)
+    view.ratecheckbox.setCheckState(Qt.Checked)
+    model.fpaths = [datadir.joinpath(p) for p in cfg["sigfnames"]]
+
+    # Mock the controller's batch_processor in order to avoid
+    # calls to the controller's get_wpathpeaks and get_wpathstats methods.
+    controller.methodnb = 0
+    controller.nmethods = 5
+    controller.filenb = 0
+    controller.nfiles = len(model.fpaths)
+
+    model.wdirpeaks = tmpdir
+    model.wdirstats = tmpdir
+
+    model.status = 'processing files'
+    model.plotting = False
+    model.progress_changed.connect(controller.dispatcher)
+
+    # Initiate batch processing.
+    controller.dispatcher(1)
+
+    # Wait for all files to be processed.
+    while controller.filenb < controller.nfiles:
+        qtbot.wait(500)
+
+    # Load each peak file saved during batch processing and assess if
+    # peaks have been identified correctly.
+    for sigfname, peaksum in zip(cfg["sigfnames"], cfg["peaksums"]):
+        with qtbot.waitSignal(model.signal_changed, timeout=5000):
+            controller.read_signal(path=datadir.joinpath(sigfname))
+        fname = Path(sigfname).stem
+        model.rpathpeaks = tmpdir.join(f"{fname}_peaks.csv")
+        with qtbot.waitSignal(model.peaks_changed, timeout=5000):
+            controller.read_peaks()
+        assert sum(model.peaks) == peaksum
+        model.reset()
+
+    # Load each stats file saved during batch processing and assess if
+    # stats have been caclualted correctly.
+    for sigfname, stat in zip(cfg["sigfnames"], cfg["stats"]):
+        fname = Path(sigfname).stem
+        statsfname = tmpdir.join(f"{fname}_stats.csv")
+        stats = pd.read_csv(statsfname)
+        assert np.around(stats["period"].mean(), 4) == stat[0]
+        assert np.around(stats["rate"].mean(), 4) == stat[1]
